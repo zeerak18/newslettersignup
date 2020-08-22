4d2aab2b51ac2e5c735eaba89514b4da90745f08
100554495 <zeerak.siddiqui@ontariotechu.net>
2020-08-22 17:57:20 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us17.api.mailchimp.com/3.0/lists/3c09526965"
+  const options = {
+    method: "POST",
+    auth: "zeerak1:e44f4800bd3bb9862341ad29b1d12697-us17"
+  }
+
+  const request = https.request(url, options, function(response){
+
+    if (response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html")
+    } else {
+      res.send(__dirname + "/failure.html");
+    };
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+
+
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
+
+app.listen(3000, function(){
+  console.log("Server has started listening on port 3000!");
+})
+
+
+
+// API Key - Mail Chimp
+// e44f4800bd3bb9862341ad29b1d12697-us17
+// List ID
+// 3c09526965
